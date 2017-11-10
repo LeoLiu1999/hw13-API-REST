ac67bcae22b591bc819fc3655f06cc2dfb9d592b
TakingTheL <yliu6@stuy.edu>
2017-11-09 20:55:24 -0500

    done

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 import json
 import urllib2
 
+u_resp = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=am0ATrYMJQfY8wXWMl4Jfv2OBJGZtFyaeQN5Qy8l")
+json_string = u_resp.read()
+json_dict = json.loads(json_string)
+print json_dict
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    u_resp = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=am0ATrYMJQfY8wXWMl4Jfv2OBJGZtFyaeQN5Qy8l")
-    json_string = u_resp.read()
-    json_dict = json.loads(json_string)
-    return render_template(dict=json_dict)
+    return render_template("temp.html", dict=json_dict)
 
 if (__name__ == "__main__"):
     app.debug = True
