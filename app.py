4ff56596b98104adf42eb12a405d7143422d1a8d
TakingTheL <yliu6@stuy.edu>
2017-11-14 00:18:02 -0500

    added new york times top article

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ json_string = u_resp.read()
 json_dict = json.loads(json_string)
 print json_dict
 
+u_resp2 = urllib2.urlopen("http://api.nytimes.com/svc/topstories/v2/technology.json?api-key=33106c01bb1a4ca9a76239620531eb30")
+json_string2 = u_resp2.read()
+json_dict2 = json.loads(json_string2)
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("temp.html", dict=json_dict)
+    return render_template("temp.html", dict=json_dict, dict2=json_dict2)
 
 if (__name__ == "__main__"):
     app.debug = True
